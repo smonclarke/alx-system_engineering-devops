a0e2eb9e2283a623862010911ad3ca9eafd4eed3
smonclarke <112950416+smonclarke@users.noreply.github.com>
2023-10-25 13:26:09 +0100

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# rename `phpp` extensions to `php`
-
-exec { 'fix_phpp':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
-}
